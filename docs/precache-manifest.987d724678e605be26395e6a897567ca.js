38915bdaaa4454e3f6128048828b56f7f2c1b785
Nelson Martins <nelsonjmartins@gmail.com>
2020-04-19 13:13:37 +0100

    build deployed

---
+++ docs/precache-manifest.987d724678e605be26395e6a897567ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722ba238c2c1f30b4b824ebb9e6cc66a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "255135ce657292191fd5",
+    "url": "./static/css/main.61d63a14.chunk.css"
+  },
+  {
+    "revision": "a7f4c40eb146df90ec3d",
+    "url": "./static/js/2.0b638dbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0b638dbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "255135ce657292191fd5",
+    "url": "./static/js/main.830254b4.chunk.js"
+  },
+  {
+    "revision": "cdebb7ffe1d89f25da10",
+    "url": "./static/js/runtime-main.b5b982bb.js"
+  }
+]);
