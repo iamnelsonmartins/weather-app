c658d51daa950a62c7acb23d1845e407e44aa7f4
iamnelsonmartins <nelsonjmartins@gmail.com>
2020-12-14 16:27:35 +0000

    fix output of hour to display 0 if value < 10

--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -1,33 +1,35 @@
-import React from "react";
+import React from 'react'
 
 const HourlyForecast = ({ data }) => {
+  const getHour = (time) => {
+    let finalHour = time ? new Date(time).getHours() : new Date().getHours()
+    if (finalHour < 10) {
+      finalHour = '0' + finalHour
+    }
+    return finalHour
+  }
 
-    const getHour = time => time ? new Date(time).getHours() : new Date().getHours();
-
-    const displayMoreInfo = (data, index) => {
-        return (
-            <div key={index}>
-                <div className='weather-details'>
-                    <div className='weather-details-data'>
-                        {`${getHour(data.dt * 1000 - 1000)}:00`}
-                    </div>
-                    <div className='weather-details-data'>-</div>
-                    <div className='weather-details-data'>
-                        <strong>{`${Math.round(data.main.temp_max)}°`} </strong>{`${Math.round(data.main.temp_min)}°`}
-                    </div>
-                    <span className='spacer' />
-                </div>
-            </div>
-        );
-    };
-
+  const displayMoreInfo = (data, index) => {
     return (
-        <div className="weather-hour">
-            {data.map((data, index) => (
-                displayMoreInfo(data, index)
-            ))}
+      <div key={index}>
+        <div className='weather-details'>
+          <div className='weather-details-data'>
+            <span>{`${getHour(data.dt * 1000 - 1000)}:00`}</span>
+            <span> - </span>
+            <strong>{`${Math.round(data.main.temp_max)}°`} </strong>
+            {`${Math.round(data.main.temp_min)}°`}
+          </div>
+          <span className='spacer' />
         </div>
-    );
-};
+      </div>
+    )
+  }
+
+  return (
+    <div className='weather-hour'>
+      {data.map((data, index) => displayMoreInfo(data, index))}
+    </div>
+  )
+}
 
 export default HourlyForecast
